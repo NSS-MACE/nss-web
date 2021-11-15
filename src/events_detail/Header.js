159ae100a2593fca10e5aa012d8fe710efa8e086
Treesa Jose <treesajisjoy3280@gmail.com>
2021-11-15 09:20:26 +0530

    size adjust

--- src/events_detail/Header.js
+++ src/events_detail/Header.js
@@ -1,6 +1,89 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export default function Header(){
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  const scrollto = (el) => {
+    let header = select('#header');
+    let offset = header.offsetHeight;
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  useEffect(()=>{
+    let selectHeader = select('#header');
+    if (selectHeader) {
+      let headerOffset = selectHeader.offsetTop;
+      let nextElement = selectHeader.nextElementSibling;
+      const headerFixed = () => {
+        if ((headerOffset - window.scrollY) <= 0) {
+          selectHeader.classList.add('fixed-top')
+          nextElement.classList.add('scrolled-offset')
+        } else {
+          selectHeader.classList.remove('fixed-top')
+          nextElement.classList.remove('scrolled-offset')
+        }
+      }
+      window.addEventListener('load', headerFixed)
+      onscroll(document, headerFixed)
+    }
+
+    on('click', '.mobile-nav-toggle', function(e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+    })
+  
+    on('click', '.navbar .dropdown > a', function(e) {
+      if (select('#navbar').classList.contains('navbar-mobile')) {
+        e.preventDefault()
+        this.nextElementSibling.classList.toggle('dropdown-active')
+      }
+    }, true)
+  
+    on('click', '.scrollto', function(e) {
+      if (select(this.hash)) {
+        e.preventDefault()
+  
+        let navbar = select('#navbar');
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile')
+          let navbarToggle = select('.mobile-nav-toggle');
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
+        }
+        scrollto(this.hash)
+      }
+    }, true)
+  });
 
   return (
     <header id="header" className="d-flex align-items-center" style={{background:'rgba(21, 5, 23, 0.8)'}}>
