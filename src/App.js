4dfbefa5c1d37692b6694391b707a252890ae414
Treesa Jose <treesajisjoy3280@gmail.com>
2021-12-02 14:35:12 +0530

    404error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './Home';
 import Event1 from './events/Event1';
 import Event2 from './events/Event2';
@@ -14,12 +14,12 @@ function App() {
       <div>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/#/event1" component={Event1} />
-          <Route path="/#/event2" component={Event2} />
-          <Route path="/#/event3" component={Event3} />
-          <Route path="/#/event4" component={Event4} />
-          <Route path="/#/event5" component={Event5} />
-          <Route path="/#/event6" component={Event6} />
+          <Route path="/event1" component={Event1} />
+          <Route path="/event2" component={Event2} />
+          <Route path="/event3" component={Event3} />
+          <Route path="/event4" component={Event4} />
+          <Route path="/event5" component={Event5} />
+          <Route path="/event6" component={Event6} />
         </Switch>
       </div>
     </Router>
