6f001cc839d21216dcd6227db4b3bfb800de5d8b
Treesa Jose <treesajisjoy3280@gmail.com>
2021-12-02 13:05:52 +0530

    router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Event6 from './events/Event6';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Switch>
           <Route path="/" exact component={Home} />
