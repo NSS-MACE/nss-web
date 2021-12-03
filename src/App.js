dc7e33031df6ca99f8e7fe29a30280d9d54c0a56
Treesa Jose <treesajisjoy3280@gmail.com>
2021-12-04 00:56:54 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './Home';
 import Event1 from './events/Event1';
 import Event2 from './events/Event2';
@@ -10,7 +10,7 @@ import Event6 from './events/Event6';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/event1" component={Event1} />
