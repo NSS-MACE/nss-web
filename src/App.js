059f2243faa2716b5690d0a7bd018d0cffcc51e7
Treesa Jose <treesajisjoy3280@gmail.com>
2021-12-03 11:49:56 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Event6 from './events/Event6';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/nss-web">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/event1" component={Event1} />
