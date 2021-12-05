c273c8c291a8fe25e74615fb3faaf95fa6cc224c
Treesa Jose <treesajisjoy3280@gmail.com>
2021-12-05 14:54:19 +0530

    scrolling

--- src/Home.js
+++ src/Home.js
@@ -21,6 +21,7 @@ function Home() {
         <Team/>
       </main>
       <Footer/>
+      <a href="#hero" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
     </div>
   );
 }
